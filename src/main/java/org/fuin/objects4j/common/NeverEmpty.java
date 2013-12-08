fcd5e0a75a7e1f73bf4738ff31528b7e39469e86
michael-schnell <michael@fuin.org>
2013-12-08 10:29:24 +0100

    Added two post condition annotations

---
+++ src/main/java/org/fuin/objects4j/common/NeverEmpty.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2013 Future Invent Informationsmanagement GmbH. All rights
+ * reserved. <http://www.fuin.org/>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fuin.objects4j.common;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Used to express that a method's return value is never <code>null</code> or
+ * empty.
+ */
+// CHECKSTYLE:OFF
+@Documented
+@Target(value = { METHOD })
+@Retention(value = RUNTIME)
+public @interface NeverEmpty {
+}
+// CHECKSTYLE:ON
