5dbaf7f90003e8bcd4dfa800d2c41a25683c6cbf
Michael Schnell <michael@fuin.org>
2015-03-15 15:21:07 +0100

    Added missing tests

---
+++ src/test/java/org/fuin/objects4j/vo/AbstractStringValueObjectTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2013 Future Invent Informationsmanagement GmbH. All rights
+ * reserved. <http://www.fuin.org/>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fuin.objects4j.vo;
+
+import junitx.extensions.EqualsHashCodeTestCase;
+
+//CHECKSTYLE:OFF
+public class AbstractStringValueObjectTest extends EqualsHashCodeTestCase {
+
+    public AbstractStringValueObjectTest(String name) {
+        super(name);
+    }
+
+    @Override
+    protected Object createInstance() throws Exception {
+        return new TestStringVO("A");
+    }
+
+    @Override
+    protected Object createNotEqualInstance() throws Exception {
+        return new TestStringVO("B");
+    }
+    
+    /** Implementation for tests. */
+    public static class TestStringVO extends AbstractStringValueObject {
+
+        private static final long serialVersionUID = 1L;
+        
+        private final String value;
+        
+        public TestStringVO(final String value) {
+            super();
+            this.value = value;
+        }
+        
+        @Override
+        public String asBaseType() {
+            return value;
+        }
+        
+    }
+
+}
+//CHECKSTYLE:ON
